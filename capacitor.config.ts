b25cd4d0a758fa23cf0e3fe5b93f05cdb6b336c3
rebecabraga007-cmyk <rebeca.braga007@gmail.com>
2026-05-06 11:05:25 -0300

    Add Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.maridaas.app',
+  appName: 'Maridaas',
+  webDir: 'dist'
+};
+
+export default config;
